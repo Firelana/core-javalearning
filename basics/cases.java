aaec30ea96591873184c62271c8a0a6489af84cc
Firelana <87412814+Firelana@users.noreply.github.com>
2025-11-19 01:30:40 +0530

    Create cases.java

---
+++ basics/cases.java
@@ -0,0 +1,19 @@
+public class HelloWorld
+{
+    public static void main(String args[])
+    {
+      String name = "Atharva";
+      int value = name name.lenght();
+        System.out.println(value);
+
+   int value2 = name name.toLowerCase();
+        System.out.println(value2);
+
+   int value3 = name name.toUpperCase();
+        System.out.println(value2);
+
+    String nonTrimmedString = "    Atharva   ";
+      string trimmedstring = nonTrimmedString.trim();
+      System.out.println(nonTrimmedString.trim());
+    }
+}
