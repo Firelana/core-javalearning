b322361807302d6e311646e41e9e0ef098e27ec5
Firelana <87412814+Firelana@users.noreply.github.com>
2025-11-19 01:09:17 +0530

    Create Constants.java
    Constants are like a variable, except that their value never changes during program execution.

---
+++ basics/Constants.java
@@ -0,0 +1,10 @@
+public class Constants
+  public static void main (String[]args);
+{
+  final double L1 = 3.56;
+  System.out.println(L1);
+  // single line comment
+  /* Multi line 
+  comment */
+}
+}
