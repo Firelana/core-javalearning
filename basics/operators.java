f7450523c8589e518b89ad74dcb4598324640aa0
Firelana <87412814+Firelana@users.noreply.github.com>
2025-11-19 01:16:29 +0530

    Create operators.java

---
+++ basics/operators.java
@@ -0,0 +1,16 @@
+public class HelloWorld
+{
+    public static void main(String args[])
+    {
+      int x=110+3;
+      int x1=70-36;
+      int x2=102*35;
+      int x3=90/7;
+      int x4=12%6;
+      System.out.println(x);
+            System.out.println(x1);
+            System.out.println(x2);
+            System.out.println(x3);
+            System.out.println(x4);
+    }
+}
