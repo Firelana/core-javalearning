1fef9174b5ebbbbc2d4da9638359552e8f463825
Firelana <87412814+Firelana@users.noreply.github.com>
2025-11-19 00:58:05 +0530

    Create input.java
    taking user input

---
+++ basics/input.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class input
+{
+        public static void main(String args[])
+        {
+              Scannner sc=new Scanner(System.in);
+              String name=sc.nextLine();
+              System.out.println(name);
+              int age=sc.nextint();
+              System.out.println(age);
+        }
+}
