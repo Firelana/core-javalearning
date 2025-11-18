dd0d51ded37e8fe47f7a791fc366e0393a1e8ec6
Firelana <87412814+Firelana@users.noreply.github.com>
2025-11-19 01:04:44 +0530

    Create Primitive.java
    The eight primitives defined in Java are int, byte, short, long, float, double, boolean, and char those aren't considered objects and represent raw values.

---
+++ basics/Primitive.java
@@ -0,0 +1,13 @@
+class Primitive
+{ 
+       public static void main(String args[])
+      {
+byte age = 18;
+long var = 850.0;
+float money = 200.54;
+char word = 'a';
+int var1 = 256;
+        short var2 = 22;
+        System.out.println(age+var+money+word+var1+var2);
+      } 
+}
