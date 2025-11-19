3cb72c044b831c158194ec2bb3aabd92bd0fa3a0
Firelana <87412814+Firelana@users.noreply.github.com>
2025-11-19 17:49:10 +0530

    Create logicalops.java

---
+++ conditionals/logicalops.java
@@ -0,0 +1,30 @@
+public class testing {
+    public static void main(String[] args) {
+       boolean a = true;
+        boolean b = true;
+        if (a && b) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No.");
+        }
+
+        boolean aa = true;
+        boolean bb = true;
+        if (aa || bb) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No.");
+        }
+
+
+                boolean aaa = true;
+        boolean bbb = false;
+        if (aaa != bbb) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No.");
+        }
+    
+
+    }
+}
