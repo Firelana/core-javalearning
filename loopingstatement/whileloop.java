3aba84b96aa5f9a3348f8753fe22c67d1c2cc236
Firelana <87412814+Firelana@users.noreply.github.com>
2025-11-22 00:06:32 +0530

    Create whileloop.java

---
+++ loopingstatement/whileloop.java
@@ -0,0 +1,12 @@
+public class whileloop {
+    public static void main (String[]args) {
+        System.out.println("loop Statement");
+
+        // Starts from here.
+        int a = 1;
+        while (a<=10) {
+            System.out.println("Number print: "+ a);
+            a++;
+        }
+    }
+}
