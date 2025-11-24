32b77a4fe2aaf1091af0f6a495cff3f4e35a272d
Firelana <87412814+Firelana@users.noreply.github.com>
2025-11-25 01:09:12 +0530

    Create arrtwo.java

---
+++ array/arrtwo.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class testtwo {
+    public static void main(String[] args) {
+        Scanner ar = new Scanner(System.in);
+    String [] msg = {"Hello", "Hey", "Hii"};
+    System.out.print("Message: "+ msg[0]);
+        }
+    }
