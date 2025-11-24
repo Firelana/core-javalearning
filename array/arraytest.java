cab98754351057716c210d51e355a75a18646e1c
Firelana <87412814+Firelana@users.noreply.github.com>
2025-11-25 00:36:11 +0530

    Create arraytest.java

---
+++ array/arraytest.java
@@ -0,0 +1,9 @@
+public class arraytest {
+    public static void main(String[] args) {
+    int [] round = new int[4];
+    round[1] = 120;
+    round[2] = 150;
+    round[3] = 190;
+    System.out.println("Score: "+ (round[2]));
+        }
+    }
