7f076e89ebcb56748e6925f4b7aa80ed399034f3
Firelana <87412814+Firelana@users.noreply.github.com>
2025-11-28 01:02:57 +0530

    Create voidmethod.java
    ignore file name

---
+++ method/voidmethod.java
@@ -0,0 +1,8 @@
+public class testwo {
+    static void msg1() {
+        System.out.print("Hey, there! how are you?\n Hope you are doing well.");
+    }
+    public static void main(String[] args) {
+        msg1();
+    }
+}
