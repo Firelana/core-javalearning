298bad8e5630a1680751ce83b71ebc3ed90f3512
Firelana <87412814+Firelana@users.noreply.github.com>
2025-12-02 13:25:32 +0530

    Create pratice1.java
    ignore comments

---
+++ method/pratice1.java
@@ -0,0 +1,13 @@
+ public class testing{
+      static void starp(int a) {
+        for (int i=0; i<=a; i++){
+         for (int j=0; j<i+1;j++){
+         System.out.print("*");
+         }
+         System.out.println();
+      }
+    }
+        public static void main(String[] args) {
+        starp(4);
+  }
+}
