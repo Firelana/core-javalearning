bd9a186a57f014cb802f0ee222b17f0b85bb3118
Firelana <87412814+Firelana@users.noreply.github.com>
2025-12-08 00:33:41 +0530

    Add employee class with methods for salary and name

---
+++ OOPS/classeswithmethod.java
@@ -0,0 +1,24 @@
+class employee {
+    int salary;
+    String name;
+
+    public int getsalary() {
+        return salary;
+    }
+
+    public String getname() {
+        return name;
+    }
+
+    public void setname(String n) {
+        name = n;
+    }
+}
+
+public class testing {
+    public static void main(String[] args) {
+        employee e = new employee();
+        e.setname("Atharva");
+        System.out.println(e.getname());
+    }
+}
